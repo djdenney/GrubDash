e95cab0acf6e397b2ba0dcc77f3257e4de84fd5f
djdenney <daniel@denneythe.dev>
2021-04-10 07:50:47 -0700

    realized part of the rubric called for all middleware functions to not be arrow functions, replaced all arrow functions with named functions

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,13 +1,10 @@
 const path = require("path")
 
-// Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"))
 
-// Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId")
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
-const doesDishExist = (req, res, next) => {
+function doesDishExist(req, res, next) {
     const { dishId } = req.params
     const foundDish = dishes.find((dish) => {
         return dish.id === dishId
@@ -20,7 +17,7 @@ const doesDishExist = (req, res, next) => {
     next({ status: 404, message: `Dish does not exist: ${dishId}` })
 }
 
-const validate = (req, res, next) => {
+function validate(req, res, next) {
     const { data: { name, description, price, image_url } = {} } = req.body
     if (name && description && price && price > 0 && typeof price === 'number' && image_url) {
         return next()
@@ -34,7 +31,7 @@ const validate = (req, res, next) => {
     }
 }
 
-const create = (req, res, next) => {
+function create(req, res, next) {
     const { data: { name, description, price, image_url } = {} } = req.body
     const newId = nextId()
     const newDish = {
@@ -48,12 +45,12 @@ const create = (req, res, next) => {
     res.status(201).json({ data: newDish })
 }
 
-const read = (req, res, next) => {
+function read(req, res, next) {
     const dish = res.locals.dish
     res.json({ data: dish })
 }
 
-const update = (req, res, next) => {
+function update(req, res, next) {
     let dish = res.locals.dish
     const originalDishId = dish.id
     const { data: { id, name, description, price, image_url } = {} } = req.body
@@ -71,7 +68,7 @@ const update = (req, res, next) => {
     next({ status: 400, message: `Dish id does not match route id. Dish: ${id}, Route: ${originalDishId}` })
 }
 
-const list = (req, res, next) => {
+function list(req, res, next) {
     res.json({ data: dishes})
 }
 
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,80 +1,73 @@
-const path = require("path");
-const dishesController = require("../dishes/dishes.controller");
+const path = require("path")
 
-// Use the existing order data
-const orders = require(path.resolve("src/data/orders-data"));
+const orders = require(path.resolve("src/data/orders-data"))
 
-// Use this function to assigh ID's when necessary
-const nextId = require("../utils/nextId");
+const nextId = require("../utils/nextId")
 
-// TODO: Implement the /orders handlers needed to make the tests pass
-const orderDoesExist = (req, res, next) => {
+function orderDoesExist(req, res, next) {
     const { orderId } = req.params
     const foundOrder = orders.find((order) => order.id === orderId)
     if (foundOrder) {
         res.locals.order = foundOrder
         return next()
     }
-    next({
-        status: 404,
-        message: `Order does not exist: ${orderId}`
-    })
+    next({ status: 404, message: `Order does not exist: ${orderId}` })
 }
 
-const bodyHasDeliverToProperty = (req, res, next) => {
+function bodyHasDeliverToProperty(req, res, next) {
     const { data: { deliverTo } = {} } = req.body
     deliverTo ? next() : next({ status: 400, message: 'Order must include a deliverTo'})
 }
 
-const bodyHasMobileNumberProperty = (req, res, next) => {
+function bodyHasMobileNumberProperty(req, res, next) {
     const { data: { mobileNumber } = {} } = req.body
     mobileNumber ? next() : next({ status: 400, message: 'Order must include a mobileNumber'})
 }
 
-const bodyHasDishesProperty = (req, res, next) => {
+function bodyHasDishesProperty(req, res, next) {
     const { data: { dishes } = {} } = req.body
     dishes ? next() : next({ status: 400, message: 'Order must include a dish'})
 }
 
-const dishesPropertyIsAnArray = (req, res, next) => {
+function dishesPropertyIsAnArray(req, res, next) {
     const { data: { dishes } = {} } = req.body
     Array.isArray(dishes) ? next() : next({ status: 400, message: 'Order must include at least one dish'})
 }
 
-const dishesArrayIsNotEmpty = (req, res, next) => {
+function dishesArrayIsNotEmpty(req, res, next) {
     const { data: { dishes } = {} } = req.body
     dishes.length > 0 ? next() : next({ status: 400, message: 'Order must include at least one dish'})
 }
 
-const dishesArrayHasDishQuantity = (req, res, next) => {
+function dishesArrayHasDishQuantity(req, res, next) {
     const { data: { dishes } = {} } = req.body
     const index = dishes.findIndex((dish) => !dish.quantity)
     index != -1 ? next({ status: 400, message: `Dish ${index} must have a quantity that is an integer greater than 0`}) : next() 
 }
 
-const dishQuantityIsGreaterThanZero = (req, res, next) => {
+function dishQuantityIsGreaterThanZero (req, res, next) {
     const { data: { dishes } = {} } = req.body
     const index = dishes.findIndex((dish) => dish.quantity <= 0)
     index != -1 ? next({ status: 400, message: `Dish ${index} must have a quantity that is an integer greater than 0`}) : next() 
 }
 
-const dishQuantityIsAnInteger = (req, res, next) => {
+function dishQuantityIsAnInteger(req, res, next) {
     const { data: { dishes } = {} } = req.body
     const index = dishes.findIndex((dish) => !Number.isInteger(dish.quantity))
     index != -1 ? next({ status: 400, message: `Dish ${index} must have a quantity that is an integer greater than 0`}) : next() 
 }
 
-const statusIsMissing = (req, res, next) => {
+function statusIsMissing(req, res, next) {
     const { data: { status } = {} } = req.body
     status ? next() : next({ status: 400, message: `Order must have a status of pending, preparing, out-for-delivery, delivered` })
 }
 
-const statusIsEmpty = (req, res, next) => {
+function statusIsEmpty(req, res, next) {
     const { data: { status } = {} } = req.body
     status != '' ? next() : next({ status: 400, message: `Order must have a status of pending, preparing, out-for-delivery, delivered` })
 }
 
-const statusIsInvalid = (req, res, next) => {
+function statusIsInvalid(req, res, next) {
     const { data: { status } = {} } = req.body
     status === 'pending' || 
     status === 'preparing' || 
@@ -83,7 +76,7 @@ const statusIsInvalid = (req, res, next) => {
     next({ status: 400, message: `Order must have a status of pending, preparing, out-for-delivery, delivered` })
 }
 
-const dataIdDoesNotMatchOrderId = (req, res, next) => {
+function dataIdDoesNotMatchOrderId(req, res, next) {
     const { data: { id } = {} } = req.body
     const order = res.locals.order
     if (!id || id === '' || id === null || id === undefined) {
@@ -92,7 +85,7 @@ const dataIdDoesNotMatchOrderId = (req, res, next) => {
     id === order.id ? next() : next({ status: 400, message: `Order id does not match route id. Order: ${id}, Route: ${order.id}` })
 }
 
-const create = (req, res, next) => {
+function create(req, res, next) {
     const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body
     const newOrderId = nextId()
     const newOrder = {
@@ -106,13 +99,13 @@ const create = (req, res, next) => {
     res.status(201).json({ data: newOrder })
 }
 
-const read = (req, res, next) => {
+function read(req, res, next) {
     const order = res.locals.order
     res.json({ data: order })
 
 }
 
-const update = (req, res, next) => {
+function update(req, res, next) {
     const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body
     const order = res.locals.order
     const updatedOrder = {
@@ -125,7 +118,7 @@ const update = (req, res, next) => {
     res.json({ data: updatedOrder })
 }
 
-const remove = (req, res, next) => {
+function remove(req, res, next) {
     const order = res.locals.order
     const { orderId } = req.params
     const index = orders.findIndex((order) => order.id === orderId)
@@ -140,7 +133,7 @@ const remove = (req, res, next) => {
     res.sendStatus(204)
 }
 
-const list = (req, res, next) => {
+function list(req, res, next) {
     res.json({ data: orders })
 }
 
